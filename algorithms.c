ee18d516ff2d9cc5a39f9a8b2c6da6d5659abead
loreson-953 <loreson-953@github.com>
2026-02-28 11:09:08 -0800

    Created the base for the algorithms.

---
+++ algorithms.c
@@ -0,0 +1,59 @@
+#include "algorithms.h"
+
+// Merge
+
+void merge_sort(int array[], int length) {
+	merge_sort_recursion(array, 0, length - 1);
+}
+
+void merge_sort_recursion(int array[], int left, int right) {
+	if (left < right) {
+		int midpoint = left + (right - left) / 2;
+
+		merge_sort_recursion(array, left, midpoint);
+		merge_sort_recursion(array, midpoint + 1, right);
+
+		merge_sorted_arrays(array, left, midpoint, right);
+	}
+}
+
+void merge_sorted_arrays(int array[], int left, int midpoint, int right) {
+	int left_length = midpoint - left + 1;
+	int right_length = right - midpoint;
+	int temp_left[left_length];
+	int temp_right[right_length];
+
+	int i, j, k;
+	
+	for (i = 0; i < left_length; i++)
+		temp_left[i] = array[left + i];
+	for (i = 0; i < left_length; i++)
+		temp_right[i] = array[midpoint + 1 + i];
+
+	for (i = 0, j = 0, k = left; k <= right; k++) {
+		if ((i < left_length) &&
+			(j >= right_length || temp_left[i] <= temp_right[j])) {
+			array[k] = temp_left[i];
+			i++;
+		} else {
+			array[k] = temp_right[j];
+			j++;
+		}
+	}
+}
+
+// Bubble
+
+void bubble_sort(int array[], int length) {
+	for (int i = 0; i < length - 1; i++) {
+		int temp = 0;
+
+		for (int j = 0; j < length - i - 1; j++) {
+			if (array[j] > array[j + 1]) {
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+}
---
+++ algorithms.h
@@ -0,0 +1,14 @@
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+// Merge
+
+void merge_sort(int array[], int length);
+void merge_sort_recursion(int array[], int left, int right);
+void merge_sorted_arrays(int array[], int left, int midpoint, int right);
+
+// Bubble
+
+void bubble_sort(int array[], int length);
+
+#endif
