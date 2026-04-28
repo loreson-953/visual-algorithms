34d8d5e7a1212b056860fbaaa0d9fcf73c8a485f
loreson-953 <allenajohnston953@gmail.com>
2026-04-27 20:51:10 -0700

    Readded algorithm headers.

---
+++ algorithms.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "algorithms.h"
+
+void bubble_sort(int array[], int length) {	
+	for (int k = 0; k < length; k++) printf("%d ", array[k]);
+	printf("\n");
+	
+	for (int i = 0; i < length - 1; i++) {
+		int temp = 0;
+		
+		
+		for (int j = 0; j < length - i - 1; j++) {
+			if (array[j] > array[j + 1]) {
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}	
+
+			for (int k = 0; k < length; k++) printf("%d ", array[k]);
+			printf("\n");
+		}		
+	}
+}
---
+++ algorithms.h
@@ -0,0 +1,6 @@
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+void bubble_sort(int array[], int length);
+
+#endif
