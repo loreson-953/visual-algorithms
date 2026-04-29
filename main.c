9e68c5a39f48431439d57ea0fced3a403803ed80
loreson-953 <allenajohnston953@gmail.com>
2026-04-29 10:22:46 -0700

    General changes

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ int starty = 0;
 char *choices[] =
 	{
 		"1. Bubble Sort",
-		"0. Exit"
+		"0. Exit",
 	};
 
 int n_choices = sizeof(choices) / sizeof(char *);
@@ -77,10 +77,12 @@ int main(void) {
 						if (choice == -1)
 							goto end;
 
-						mvprintw(22, 1, "Choice made is: %d
-						
-						mvprintw(22, 1, "Choice 
+						mvprintw(22, 1, "Choice made is: %d", choice, choices[choice - 1]);
+						refresh();
 					}
+
+					print_menu(menu_win, choice);
+					break;
 				}
 			}
 	}
@@ -97,15 +99,14 @@ void print_menu(WINDOW *menu_win, int highlight) {
 
 	box(menu_win, 0, 0);
 
-	for (int i = 0; i < n_choices; i++) {
+	for (int i = 0; i < n_choices; ++i) {
 		if (highlight == i + 1) {
 			wattron(menu_win, A_REVERSE);
 			mvwprintw(menu_win, y, x, "%s", choices[i]);
 			wattroff(menu_win, A_REVERSE);
-		} else {
-			mvwprintw(menu_win, y, x, "%s", choices[i]);
-			y++;
-		}
+		} else mvwprintw(menu_win, y, x, "%s", choices[i]);
+		y++;
+
 	}
 	wrefresh(menu_win);
 	
@@ -115,15 +116,13 @@ void report_choice(int mouse_x, int mouse_y, int *p_choice) {
 	int i = startx + 2;
 	int j = starty + 3;
 
-	for (int choice = 0; choice < n_choices; choice++)
+	for (int choice = 0; choice < n_choices; ++choice)
 		if (mouse_y == j + choice
 			&& mouse_x >= i
 			&& mouse_x <= i + strlen(choices[choice]))
 			{
-				if (choice == n_choices - 1)
-					*p_choice = -1;
-				else
-					*p_choice = choice + 1;
+				if (choice == n_choices - 1) *p_choice = -1;
+				else *p_choice = choice + 1;
 				break;
 			}
 	
